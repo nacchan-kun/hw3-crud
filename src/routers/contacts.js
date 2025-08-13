06ca151123320c766a08f9e5b6d23371f4888cb0
nacchan-kun <sendono.nc17@gmail.com>
2025-08-13 22:04:02 +0300

    Update contacts.js

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,12 +1,13 @@
+
 import { Router } from 'express';
-import { getContactsController, getContactByIdController, createContactController, deleteContactController } from '../controllers/contacts.js';
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
-router.post('/contacts', ctrlWrapper(createContactController));
 import ctrlWrapper from '../utils/ctrlWrapper.js';
+import { getContactsController, getContactByIdController, createContactController, deleteContactController } from '../controllers/contacts.js';
 
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.post('/contacts', ctrlWrapper(createContactController));
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
 export default router;
