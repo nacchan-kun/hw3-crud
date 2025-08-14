731da20759ddb131b7dfa30e3e8e5b35056f7e28
nacchan-kun <sendono.nc17@gmail.com>
2025-08-14 23:01:39 +0300

    Update errorHandler.js

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,9 +1,18 @@
 
+import { HttpError } from 'http-errors';
+
 const errorHandler = (err, req, res, next) => {
-  const status = err.status || 500;
-  res.status(status).json({
-    status,
-    message: "Something went wrong",
+  if (err instanceof HttpError) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.name,
+      data: err,
+    });
+    return;
+  }
+  res.status(500).json({
+    status: 500,
+    message: 'Something went wrong',
     data: err.message,
   });
 };
