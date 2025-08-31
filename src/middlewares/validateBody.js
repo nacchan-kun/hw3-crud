c2ba54441003da7001e8d83273950d64cb1c7178
nacchan-kun <sendono.nc17@gmail.com>
2025-08-31 13:01:48 +0300

    Update validateBody.js

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -10,22 +10,3 @@ export const validateBody = (schema) => {
     next();
   };
 };
-
-export const getContactsController = async (req, res, next) => {
-  try {
-    const page = parseInt(req.query.page, 10) || 1;
-    const perPage = parseInt(req.query.perPage, 10) || 10;
-    const type = req.query.type;
-    const isFavourite = req.query.isFavourite;
-    const sortBy = req.query.sortBy;
-    const sortOrder = req.query.sortOrder || 'asc';
-    const result = await getPaginatedContacts(page, perPage, type, isFavourite, sortBy, sortOrder, req.user._id);
-    res.status(200).json({
-      status: 200,
-      message: "Successfully found contacts!",
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
